c9247db1ab7a9cb4b1c69268710e11ceedf46dea
Serhatuzn <seerhatuzun@gmail.com>
2024-10-02 01:28:47 +0300

    Pratice If-Else
    Commit successfully pushed

---
+++ week1/Practice If-Else/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PracticeIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lutfen bir sayi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Girdiginiz sayi cifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Girdiginiz sayi tektir.");
+            }
+
+        }
+    }
+}
